76853adee10d6e032415c94225e4bc203d8fa82c
ZmnSCPxj jxPCSnmZ <zmnscpxj@squareup.com>
2025-03-19 13:00:45 -0400

    libsidepool/tests/s/test_simple_bus.cpp: Add simple test for Bus.

---
+++ libsidepool/tests/s/test_simple_bus.cpp
@@ -0,0 +1,59 @@
+#if HAVE_CONFIG_H
+# include"config.h"
+#endif
+#undef NDEBUG
+#include"Sidepool/S/Bus.hpp"
+#include"Sidepool/S/Lessor.hpp"
+#include"Testing/Idler.hpp"
+#include<cassert>
+#include<cstddef>
+
+namespace {
+
+struct Example {
+};
+struct Example2 {
+};
+
+}
+
+int main() {
+	auto idler = Testing::Idler::make();
+	auto lessor = Sidepool::S::Lessor();
+	auto bus = Sidepool::S::Bus(*idler);
+
+	auto count = std::size_t(0);
+
+	bus.subscribe<Example>(lessor, [&](Example const&) {
+		++count;
+		return Sidepool::lift();
+	});
+
+	auto code = Sidepool::lift().then([&]() {
+		assert(count == 0);
+
+		return bus.raise(Example{});
+	}).then([&]() {
+		assert(count == 1);
+
+		return bus.raise(Example{});
+	}).then([&]() {
+		assert(count == 2);
+
+		/* Example2 is not same type as
+		Example1.
+		*/
+		return bus.raise(Example2{});
+	}).then([&]() {
+		assert(count == 2);
+
+		return Sidepool::lift();
+	});
+
+	idler->start(std::move(code));
+	idler->run();
+
+	assert(count == 2);
+
+	return 0;
+}
